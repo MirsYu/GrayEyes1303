f57e4b68976c4d3bdba21b1b764516ce37f57d41
MirsYu <mirsyu@foxmail.com>
2017-07-27 10:21:54 +0800

    添加项目文件。

---
+++ NetDeviceScanControl/NetDeviceScanControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetDeviceScanControl
+{
+    public partial class NetDeviceScanControl: UserControl
+    {
+        public NetDeviceScanControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
